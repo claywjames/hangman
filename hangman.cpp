4b4f1af9b6af5de6b1f4866542c0e20072a1087e
Clay James <clayw.james@gmail.com>
2016-09-09 18:53:28 -0400

    add framework

--- hangman.cpp
+++ hangman.cpp
@@ -8,27 +8,57 @@
 
 using namespace std;
 
+void playGame();
+void playTurn(string word, string &wrongGuesses, string &guessedWord, int guessesRemaining);
 string getWord();
+void drawHangman(int guessesRemaining);
 char getGuess();
 
 int main() {
     srand(time(NULL)); //create the seed for rand() using the current time
-    int i, guessesRemaining = 5;
+    
+    playGame();
+    cout << "Thanks for playing!!!" << endl;
+
+    return 0;
+}
+
+
+void playGame() {
     string word = getWord(), wrongGuesses(6, '\0');
     string guessedWord(word.length(), '_');
-    
+    int guessesRemaining = 6;
+
+    while (guessesRemaining > 0 && guessedWord != word) {
+        playTurn(word, &wrongGuesses, &guessedWord, guessesRemaining);
+        --guessesRemaining;
+    }
+
+    char playAgain;
+    cout << "Would you like to keep playing?(Y/N)" << endl;
+    cin >> playAgain;
+    if (playAgain == 'Y') playGame();
+
+}
+
+void playTurn(string word, string &wrongGuesses, string &guessedWord, int guessesRemaining) {
     system("CLS");
-    cout << word << endl;
+
+    drawHangman(guessesRemaining)
+    int i;
     for (i = 0; i < guessedWord.length(); i++) cout << guessedWord[i] << " ";
     cout << endl << "Guesses left: " << guessesRemaining << endl;
     cout << "Wrong Guesses: " << endl;
     for (i = 0; i < wrongGuesses.length(); i++)
         if (isalpha(wrongGuesses[i])) cout << wrongGuesses[i] << endl;
+
     char guess = getGuess();
-
-    return 0;
+    int correctLetterPositions = matchingLetterPositions(guess, word);
+    if (correctLetterPositions.empty()) {
+        cout << "incorrect";
+        wrongGuesses
+    }
 }
-
 
 string getWord() {
     int randChoice = rand() % 979; //random number between 0 and 978
